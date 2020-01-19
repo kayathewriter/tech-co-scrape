9f43a7ea4ecf52bd85675f59f269cb91ffbcbd68
Mikael Kaya <kayaswork@gmail.com>
2020-01-19 19:46:40 +0000

    first commit

---
+++ lib/tech/co/scrape.rb
@@ -0,0 +1,10 @@
+require "tech/co/scrape/version"
+
+module Tech
+  module Co
+    module Scrape
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tech/co/scrape/version.rb
@@ -0,0 +1,7 @@
+module Tech
+  module Co
+    module Scrape
+      VERSION = "0.1.0"
+    end
+  end
+end
